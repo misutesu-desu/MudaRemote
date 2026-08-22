9be941689d6d2ca937179d2362d3d779710900cf
misutesu-desu <abdullah.cafer@outlook.com>
2026-08-23 01:22:04 +0300

    📦 chore: package 4.9.0 beta release
    - bump version to 4.9.0-beta.8
    - refresh updater changelog and source manifest hashes
    - update single-file EXE sha256 manifest

--- mudae_bot.py
+++ mudae_bot.py
@@ -145,7 +145,7 @@ except ImportError:
 
 # Bot Identification
 BOT_NAME = "MudaRemote"
-CURRENT_VERSION = "4.9.0-beta.7"
+CURRENT_VERSION = "4.9.0-beta.8"
 
 IS_TERMUX = "TERMUX_VERSION" in os.environ or ("PREFIX" in os.environ and "com.termux" in os.environ["PREFIX"])
 
