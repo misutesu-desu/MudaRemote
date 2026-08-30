aa34f86012947e90c3682dd36f65de43c30edb30
misutesu-desu <abdullah.cafer@outlook.com>
2026-08-30 23:55:11 +0300

    🚀 chore: prepare v4.9.0-beta.18 release
    Prepare beta.18 metadata and GitHub Actions release publishing for the roll scheduler and multi-preset hotfix.

--- mudae_bot.py
+++ mudae_bot.py
@@ -145,7 +145,7 @@ except ImportError:
 
 # Bot Identification
 BOT_NAME = "MudaRemote"
-CURRENT_VERSION = "4.9.0-beta.17"
+CURRENT_VERSION = "4.9.0-beta.18"
 
 IS_TERMUX = "TERMUX_VERSION" in os.environ or ("PREFIX" in os.environ and "com.termux" in os.environ["PREFIX"])
 
